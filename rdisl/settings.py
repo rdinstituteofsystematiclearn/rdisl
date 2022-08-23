e3751e9990f0f956e2f8ec11ad15f7b1b8c2bece
rdisl <erpankaj.vishwakarma30@gmail.com>
2022-08-23 12:45:34 +0530

    Initial commit

--- rdisl/settings.py
+++ rdisl/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-08sb+v7m8rl6#%fve0g$w*opwn)xybx(am9-2jjml3m-^bni)8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','rdisl-website.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'rdisl-website.herokuapp.com']
 
 # Application definition
 
@@ -121,6 +121,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
